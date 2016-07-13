8fbc2bb227de78aff0e55fec7c37da811f90eba3
ivan <ivan@grishaev.me>
2016-07-13 20:47:27 +0300

    collections are documented

--- f/collection.py
+++ f/collection.py
@@ -12,117 +12,97 @@ __all__ = (
     'D',
 )
 
+range = six.moves.range
 
-class Seq(object):
+
+class SeqMixin(object):
     """
-    Todo
+    A basic mixin for all types of collections.
+
+    It provides additional methods and changes
+    collection's behaviour.
     """
 
     def join(self, sep=""):
+        """
+        Joins a collection into a string.
+
+        Note: turn items to strings before using map:
+        >>> coll.map(str).join(',')
+        """
         return sep.join(self)
 
-    # lt todo
-    def reversed(self):
-        return self.cls(reversed(self))
-
     def foreach(self, func, *args, **kwargs):
+        """
+        Performs a function with additional arguments
+        for each element without returning a result.
+        """
         for item in self:
             func(item, *args, **kwargs)
 
     def map(self, fn, *args, **kwargs):
+        """
+        Returns a new collection of this type
+        without changing an existing one.
 
+        Each element is a result of applying a passed function
+        to corresponding element of an old collection.
+        """
         def process(item):
             return fn(item, *args, **kwargs)
 
-        return self.cls(map(process, self))
+        return self.__class__(map(process, self))
 
     def filter(self, pred, *args, **kwargs):
 
         def criteria(item):
             return pred(item, *args, **kwargs)
 
-        return self.cls(filter(criteria, self))
+        return self.__class__(filter(criteria, self))
 
     def reduce(self, init, fn, *args, **kwargs):
-
+        """
+        Returns a new filtered collection.
+        """
         def reducer(res, item):
             return fn(res, item, *args, **kwargs)
 
         return six.moves.reduce(reducer, self, init)
 
-    # lt todo
-    def sorted(self, key=None):
-        return self.cls(sorted(self, key=key))
-
-    # lt todo
-    def distinct(self):
-
-        cache_hash = set()
-        cache_list = []
-
-        def is_hashable(x):
-            return getattr(x, '__hash__', None) is not None
-
-        def cache_set(x):
-            if is_hashable(x):
-                cache_hash.add(x)
-            else:
-                cache_list.append(x)
-
-        def cache_has(x):
-            if is_hashable(x):
-                return x in cache_hash
-            else:
-                return x in cache_list
-
-        res = []
-
-        def process(x):
-            if not cache_has(x):
-                cache_set(x)
-                res.append(x)
-
-        self.foreach(process)
-
-        return self.cls(res)
-
-    # lt todo
-    def apply(self, fn):
-        return fn(*self)
-
     def sum(self):
+        """
+        Adds all the elements together.
+
+        Turn items to numbers first using map:
+        >>> coll.map(int).sum()
+        """
         return sum(self)
 
-    # lt
-    def group(self, n=2):
-        range = six.moves.range
-        gen = (self[i: i+n] for i in range(0, len(self), n))
-        return self.cls(gen)
-
     def __add__(self, other):
+        """
+        Adds a two collection together. Returns a new collection
+        without changing old ones.
 
+        The other collection might be a different type.
+        It will be converted to the current type.
+        """
         def gen():
-
             for x in self:
                 yield x
-
-            for y in self.cls(other):
+            for y in self.__class__(other):
                 yield y
 
-        return self.cls(gen())
+        return self.__class__(gen())
 
     def __repr__(self):
-        to_repr = self.super.__repr__
-        return "%s%s" % (self.cls.__name__, to_repr())
+        """
+        Just like a parent __repr__, but with a leading prefix:
+        >>> List[1, 2, 3], Dict{1: 2, 3: 4}
+        """
+        old_repr = super(SeqMixin, self).__repr__()
+        return "%s%s" % (self.__class__.__name__, old_repr)
 
-    @property
-    def super(self):
-        return super(Seq, self)
-
-    @property
-    def cls(self):
-        return self.__class__
-
+    # Shortcuts to turn a collection into another type.
     def Tuple(self):
         return Tuple(self)
 
@@ -165,6 +145,74 @@ class LTmixin(object):
         result = super(LTmixin, self).__getslice__(*args)
         return self.__class__(result)
 
+    def reversed(self):
+        """
+        Returns a new collection in a reserved order.
+        """
+        return self.__class__(reversed(self))
+
+    def sorted(self, key=None):
+        """
+        Returns a new sorted collection.
+
+        Without passing a key functions, it returns as-is.
+        A key function is a two-argument function that determes
+        sorting logic.
+        """
+        return self.__class__(sorted(self, key=key))
+
+    def group(self, n=2):
+        """
+        Returns a new collections with items grouped by N:
+        >>> L[1, 2, 3].group(2) ==> L[L[1, 2], L[3]]
+        """
+        gen = (self[i: i+n] for i in range(0, len(self), n))
+        return self.__class__(gen)
+
+    def distinct(self):
+        """
+        Returns a new collection without duplicates.
+
+        We don't use set(...) trick here to keep previous order.
+        It supports non-hashable elements such as dicts or lists.
+        """
+
+        cache_hash = set()
+        cache_list = []
+
+        def is_hashable(x):
+            return getattr(x, '__hash__', None) is not None
+
+        def cache_set(x):
+            if is_hashable(x):
+                cache_hash.add(x)
+            else:
+                cache_list.append(x)
+
+        def cache_has(x):
+            if is_hashable(x):
+                return x in cache_hash
+            else:
+                return x in cache_list
+
+        res = []
+
+        def process(x):
+            if not cache_has(x):
+                cache_set(x)
+                res.append(x)
+
+        self.foreach(process)
+
+        return self.__class__(res)
+
+    def apply(self, fn):
+        """
+        Passes all the items as *args to a function
+        and gets the result.
+        """
+        return fn(*self)
+
 
 class LTSMeta(type):
 
@@ -187,15 +235,15 @@ class LTSmixin(object):
     pass
 
 
-class List(Seq, LTSmixin, LTmixin, list):
+class List(SeqMixin, LTSmixin, LTmixin, list):
     pass
 
 
-class Tuple(Seq, LTSmixin, LTmixin, tuple):
+class Tuple(SeqMixin, LTSmixin, LTmixin, tuple):
     pass
 
 
-class Set(Seq, LTSmixin, set):
+class Set(SeqMixin, LTSmixin, set):
     pass
 
 
@@ -219,7 +267,7 @@ class DictMeta(type):
 
 
 @six.add_metaclass(DictMeta)
-class Dict(Seq, dict):
+class Dict(SeqMixin, dict):
 
     def __iter__(self):
         """
